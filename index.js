8d2fd050f554874227a9dc5c19987e358dc0cc61
jkol7 <jkolcun7@gmail.com>
2022-06-22 19:15:41 -0400

    Added homepage links to every card.

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const showURL = 'https://api.themoviedb.org/3/tv/'
 let headingCollection = document.querySelectorAll('h4')
 let imageCollection = document.querySelectorAll('img.poster')
 let imageNetworkCollection = document.querySelectorAll('img.networkIMG')
+let linkCollection = document.querySelectorAll('a.showLink')
 
+console.log(linkCollection)
 let networkArr = []
 let networkArrImg = []
+let linkArr = []
 
 let firstData;
 
@@ -31,7 +34,9 @@ fetch(url)
       
     .then(res => res.json()) 
       .then(data => {
+
         console.log(data)
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[1].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -39,6 +44,8 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[2].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -46,6 +53,8 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[3].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -53,6 +62,8 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[4].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -61,13 +72,29 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[5].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
       })
 
+
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
+        networkArrImg.push(data.networks[0].logo_path)
+        return fetch(showURL + firstData.results[6].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
+
+      })
+
+
+
+      .then(res => res.json()) 
+      .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[7].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -75,6 +102,9 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
+
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[8].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -82,6 +112,9 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
+
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[9].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -90,6 +123,8 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[10].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -98,6 +133,8 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[11].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
@@ -105,18 +142,14 @@ fetch(url)
 
       .then(res => res.json()) 
       .then(data => {
+
+        linkArr.push(data.homepage)
         networkArrImg.push(data.networks[0].logo_path)
         return fetch(showURL + firstData.results[12].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
 
       })
 
-      .then(res => res.json()) 
-      .then(data => {
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[13].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
+   
 
       /* Begin adding to the DOM */
 
@@ -124,14 +157,15 @@ fetch(url)
       .then(res => res.json()) 
       .then(data => {
         
-
+        console.log(linkArr)
         networkArrImg.forEach((obj,i) => {imageNetworkCollection[i].src = 'https://image.tmdb.org/t/p/w500' + networkArrImg[i]})
+        linkArr.forEach((obj,i) => {linkCollection[i].href = linkArr[i]})
+
 
         firstData.results.forEach((obj, i) => {
-          
+
           headingCollection[i].innerText = obj.name
           imageCollection[i].src = 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
-          
         })
 
   
