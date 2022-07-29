14ccae1bc16d60eca80971fb20ca57957e9968c1
jkol7 <jkolcun7@gmail.com>
2022-07-29 16:47:48 -0400

    Refactored javascript with map and iterating through fetch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = 'https://api.themoviedb.org/3/trending/tv/day?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0'
+const trendingShowsURL = 'https://api.themoviedb.org/3/trending/tv/day?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0'
 
 const showURL = 'https://api.themoviedb.org/3/tv/'
 
@@ -7,176 +7,46 @@ let imageCollection = document.querySelectorAll('img.poster')
 let imageNetworkCollection = document.querySelectorAll('img.networkIMG')
 let linkCollection = document.querySelectorAll('a.showLink')
 
-console.log(linkCollection)
-let networkArr = []
-let networkArrImg = []
-let linkArr = []
-
-let firstData;
+let trendingShowNames, trendingShowImages, trendingShowIDs
+let trendingShowLinks = [], trendingShowNetworks = []
 
 
 
-fetch(url)
-    .then(res => res.json()) 
-      .then(data => {
-        console.log(data)
-          firstData = data
-          return fetch(showURL + data.results[0].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-        })
-
-
-
-      /* Fetches to call new API URL
-      This is a separate call than original url
-      Requires the show ID pulled from url to call */
-      
-      
-    .then(res => res.json()) 
-      .then(data => {
-
-        console.log(data)
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[1].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[2].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[3].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[4].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[5].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[6].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[7].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[8].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[9].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[10].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[11].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-      .then(res => res.json()) 
-      .then(data => {
-
-        linkArr.push(data.homepage)
-        networkArrImg.push(data.networks[0].logo_path)
-        return fetch(showURL + firstData.results[12].id + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US');
-
-      })
-
-   
-
-      /* Begin adding to the DOM */
-
-
-      .then(res => res.json()) 
-      .then(data => {
-        
-   
-        networkArrImg.forEach((obj,i) => {imageNetworkCollection[i].src = 'https://image.tmdb.org/t/p/w500' + networkArrImg[i]})
-        linkArr.forEach((obj,i) => {linkCollection[i].href = linkArr[i]})
-
-
-        firstData.results.forEach((obj, i) => {
-
-          headingCollection[i].innerText = [i + 1] + '. ' + obj.name
-          imageCollection[i].src = 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
-        })
+fetch(trendingShowsURL)
+  .then(res => res.json())
+  .then(data => {
+  trendingShowNames = data.results.map(el => el.name)
+  trendingShowImages = data.results.map(el => el.poster_path)
+  trendingShowIDs = data.results.map(el => el.id)
 
   
-      })
+
+  for (let i = 0; i < 12; i++){
+  imageCollection[i].src = 'https://image.tmdb.org/t/p/w500/' + trendingShowImages[i]
+  headingCollection[i].innerHTML = trendingShowNames[i]
+}
+
+for (let i = 0; i < 12; i++){
+  fetch(showURL + trendingShowIDs[i] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+  .then(res => res.json())
+  .then(data => 
+    {
+
+      //Checks if there is a logo for the network in the data. If there isn't, it will result in a 404. Instead it will hide the logo.
+      data.networks[0] ? trendingShowNetworks.push(data.networks[0].logo_path) : trendingShowNetworks.push("notfound")
+      trendingShowNetworks[i].includes('notfound') ? imageNetworkCollection[i].classList.add('hide') : imageNetworkCollection[i].src = 'https://image.tmdb.org/t/p/w500' + `${trendingShowNetworks[i]}` 
       
       
+      linkCollection[i].href = `${trendingShowLinks[i]}`
+      
+    }
+    
+    )
+  
+  }
 
+  }).catch(
 
-
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-
-
+    console.log(error)
+    
+)
