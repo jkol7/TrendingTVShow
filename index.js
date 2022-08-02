f9eec4b0572cd0db22b0e9d5d1e23bb03bb8d8bf
jkol7 <jkolcun7@gmail.com>
2022-08-02 17:30:13 -0400

    Reverted back to longer code but works better.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ let trendingShowLinks = [], trendingShowNetworks = []
 fetch(trendingShowsURL)
   .then(res => res.json())
   .then(data => {
+
   trendingShowNames = data.results.map(el => el.name)
   trendingShowImages = data.results.map(el => el.poster_path)
   trendingShowIDs = data.results.map(el => el.id)
@@ -25,28 +26,110 @@ fetch(trendingShowsURL)
   imageCollection[i].src = 'https://image.tmdb.org/t/p/w500/' + trendingShowImages[i]
   headingCollection[i].innerHTML = trendingShowNames[i]
 }
+    
 
-for (let i = 0; i < 12; i++){
-  fetch(showURL + trendingShowIDs[i] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+
+  fetch(showURL + trendingShowIDs[0] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
   .then(res => res.json())
   .then(data => 
     {
+      linkCollection[0].href = `${data.homepage}`
+      imageNetworkCollection[0].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+    })
 
-      //Checks if there is a logo for the network in the data. If there isn't, it will result in a 404. Instead it will hide the logo.
-      data.networks[0] ? trendingShowNetworks.push(data.networks[0].logo_path) : trendingShowNetworks.push("notfound")
-      trendingShowNetworks[i].includes('notfound') ? imageNetworkCollection[i].classList.add('hide') : imageNetworkCollection[i].src = 'https://image.tmdb.org/t/p/w500' + `${trendingShowNetworks[i]}` 
-      
-      
-      linkCollection[i].href = `${trendingShowLinks[i]}`
-      
-    }
-    
-    )
-  
-  }
 
-  }).catch(
+    fetch(showURL + trendingShowIDs[1] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+    .then(res => res.json())
+    .then(data => 
+      {
 
-    console.log(error)
-    
-)
+        linkCollection[1].href = `${data.homepage}`
+        imageNetworkCollection[1].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+      })
+
+
+      fetch(showURL + trendingShowIDs[2] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+      .then(res => res.json())
+      .then(data => 
+        {
+          linkCollection[2].href = `${data.homepage}`
+          imageNetworkCollection[2].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+
+        })
+
+
+        fetch(showURL + trendingShowIDs[3] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+        .then(res => res.json())
+        .then(data => 
+          {
+            linkCollection[3].href = `${data.homepage}`
+            imageNetworkCollection[3].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+          })
+
+
+          fetch(showURL + trendingShowIDs[4] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+          .then(res => res.json())
+          .then(data => 
+            {
+              linkCollection[4].href = `${data.homepage}`
+              imageNetworkCollection[4].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+            })
+
+
+            fetch(showURL + trendingShowIDs[5] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+            .then(res => res.json())
+            .then(data => 
+              {
+                linkCollection[5].href = `${data.homepage}`
+                imageNetworkCollection[5].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+              }) 
+
+            fetch(showURL + trendingShowIDs[6] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+            .then(res => res.json())
+            .then(data => 
+              {
+              linkCollection[6].href = `${data.homepage}`
+              imageNetworkCollection[6].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+              })  
+
+              fetch(showURL + trendingShowIDs[7] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+              .then(res => res.json())
+              .then(data => 
+                {
+                linkCollection[7].href = `${data.homepage}`
+                imageNetworkCollection[7].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+                })  
+                
+            fetch(showURL + trendingShowIDs[8] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+            .then(res => res.json())
+            .then(data => 
+              {
+              linkCollection[8].href = `${data.homepage}`
+              imageNetworkCollection[8].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+              })  
+              
+              fetch(showURL + trendingShowIDs[9] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+              .then(res => res.json())
+              .then(data => 
+                {
+                linkCollection[9].href = `${data.homepage}`
+                imageNetworkCollection[9].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+                }) 
+
+                fetch(showURL + trendingShowIDs[10] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+                .then(res => res.json())
+                .then(data => 
+                  {
+                  linkCollection[10].href = `${data.homepage}`
+                  imageNetworkCollection[10].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+                  }) 
+
+
+                  fetch(showURL + trendingShowIDs[11] + '?api_key=9a9ba9f5d574cc3cf69f3615a1f424e0&language=en-US')
+                  .then(res => res.json())
+                  .then(data => 
+                    {
+                    linkCollection[11].href = `${data.homepage}`
+                    imageNetworkCollection[11].src = 'https://image.tmdb.org/t/p/w500' + `${data.networks[0].logo_path}`
+                    })     
+    })
